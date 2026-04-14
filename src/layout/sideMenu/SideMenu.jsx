0c397c80cce34e50e776b9587e226f7b0a4f4d4a
Guilherme <guilhermebarroso166@gmail.com>
2026-04-14 20:23:12 -0300

    aside ajustado para mobile e desktop

--- src/layout/sideMenu/SideMenu.jsx
+++ src/layout/sideMenu/SideMenu.jsx
@@ -3,7 +3,7 @@ import SideMenuItems from "./sideMenuItems/SideMenuItems";
 
 export default function SideMenu({ isOpen, onToggle, isMobile }) {
     return (
-        <aside className="w-75 pr-4 sm:pr-6">
+        <aside className={` ${!isMobile ? 'w-75 pr-4 sm:pr-6' : ''}`}>
             <SideMenuItems isOpen={isOpen} onToggle={onToggle} isMobile={isMobile}/>
         </aside>
     );
